c5b87b812ef1f2190586944f5b1bc60f60d811a5
imashwani <ashwani.p30@gmail.com>
2019-06-06 08:44:00 +0530

    added text to contact pages

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,6 @@ import {Form, Input, Icon, Card, Button, Head, Alert, message, Layout} from 'ant
 import Link from 'next/link';
 import {withRouter} from "next/router";
 import App from "../components/layouts/App";
-const back="../images/pharma.webp"
 
 class Contact extends Component {
   render(){
@@ -12,9 +11,13 @@ class Contact extends Component {
       <App >
       <div style={{padding:"80px"}}>
       
-        <h1>This is contacts page !!
+        <h1>{"\<\<"}
+		<u>This is contacts page</u>
+		{"\>\>"}
         </h1>
-        <h2>Email: </h2>
+        <h2>Email: ashwani.p30@gmail </h2>
+	<h2>Office: New Delhi, India </h2>
+	<h2>Phone:  +91-9999999999 </h2>
 
 
       </div>
